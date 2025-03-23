91821acfa07eaf52e4f9a3323a14304f8ef23eb0
K Rajasekhar <kummararajasekhar17092004@gmail.com>
2025-03-23 14:12:05 +0530

    6 day commit

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,6 @@ const CommentRoute=require("./src/routes/CommentRoute.js")
 const authRoute=require("./src/routes/authRoute.js")
 
 
-
 app.use('/api/auth',authRoute)
 app.use('/api/blogs',blogRoutes)
 app.use('/api/comments',CommentRoute)
